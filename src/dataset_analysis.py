7636c7090ddebfa7f15227498854acb3dc9433a0
Willu12 <boryskurdek@gmail.com>
2024-11-04 16:25:21 +0100

    create class for dataset stastistics

--- src/dataset_analysis.py
+++ src/dataset_analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from .data_processing import load_audio
+from .data_processing import load_audio, split_into_clips
 
 
 def plot_spectrogram(spectrogram, title="Spectrogram"):
@@ -39,6 +39,20 @@ def dataset_durations(file_list):
         durations.append(len(audio) / sr)
     return durations
 
+def dataset_clips(file_list):
+    """
+    Returns list of track clips from .wav file list.
+    
+    Parameters:
+        file_list (List[str]): List of .wav file paths.
+
+    Returns:
+        List[float64]: List of track durations
+    """
+    for file in file_list:
+        audio, sr = load_audio(file)
+        clips = split_into_clips(audio, sample_rate=sr)
+
 
 def print_duration_summary(durations):
     """
@@ -54,3 +68,34 @@ def print_duration_summary(durations):
     print(f"Total duration: {sum(durations):.2f} sec")
     print(f"Average duration: {np.mean(durations):.2f} sec")
     print(f"Duration range: {min(durations):.2f} - {max(durations):.2f} sec")
+
+def statistics(data_files):
+    """
+    Returns statsitics of given list of files, such as total coun, mean and range.
+
+    Parameters:
+        file_list (List[str]): List of file paths in the dataset.
+
+    Returns:
+        DatasetStatistics
+    """
+    return DatasetStatistics(data_files)
+
+class DatasetStatistics:
+    def __init__(self, data_files):
+        self.data_files = data_files
+        self.durations = dataset_durations(self.data_files)
+        self.files_count = len(self.durations)
+        self.total_duration = sum(self.durations)
+        self.average_duration = np.mean(self.durations)
+        self.duration_range = [min(self.durations),max(self.durations)]
+        self.audios = [load_audio(file) for file in data_files]
+        self.audio_signals = [audio[0] for audio in self.audios]
+    
+    def __str__(self):
+        return rf"""Statstics:
+        Total files: {self.files_count},
+        Total duration: {self.total_duration:.2f} sec,
+        Average duration: {self.average_duration:.2f} sec, 
+        Duration range: {self.duration_range[0]:.2f} - {self.duration_range[0]:.2f} sec
+        """
