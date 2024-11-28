00019bc3028eb33e18a1562b3776dcd617a67a41
Willu12 <boryskurdek@gmail.com>
2024-11-28 03:14:22 +0100

    created single layer model for feature extraction

--- src/model.py
+++ src/model.py
@@ -7,6 +7,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
 class TutorialCNN(nn.Module):
     """
     A tutorial CNN model for educational purposes, suitable for general 
@@ -20,6 +21,7 @@ class TutorialCNN(nn.Module):
         fc2 (nn.Linear): Second fully connected layer.
         fc3 (nn.Linear): Output fully connected layer.
     """
+
     def __init__(self):
         super(TutorialCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
@@ -47,33 +49,45 @@ class TutorialCNN(nn.Module):
         x = self.fc3(x)
         return x
 
+
 class OriginalSizeCNN(nn.Module):
     """
     CNN for 94x128 grayscale images
     """
+
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.flattened_size = 64 * (94 // 8) * (128 // 8)
-        
+
         self.fc1 = nn.Linear(self.flattened_size, 128)
         self.fc2 = nn.Linear(128, 2)
-    
+
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
-        
+
         x = x.view(-1, self.flattened_size)
-        
+
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+
+class SingleLayerModel(nn.Module):
+    def __init__(self, layer):
+        super(SingleLayerModel, self).__init__()
+        self.layer = layer
+
+    def forward(self, x):
+        return self.layer(x)
+
